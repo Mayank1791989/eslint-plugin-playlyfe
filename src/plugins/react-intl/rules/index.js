e6b890e33f336f931980383947ca9c1e0f891f5b
Mayank Agarwal <mayank1791989@gmail.com>
2018-01-03 15:12:05 +0530

    fix(react-intl): fix react-intl rules not exported.

---
+++ src/plugins/react-intl/rules/index.js
@@ -0,0 +1,6 @@
+export default {
+  'react-intl-no-empty-translation': require('./react-intl-no-empty-translation'),
+  'react-intl-no-missing-id': require('./react-intl-no-missing-id'),
+  'react-intl-no-undef-id': require('./react-intl-no-undef-id'),
+  'react-intl-no-untranslated-string': require('./react-intl-no-untranslated-string'),
+};
